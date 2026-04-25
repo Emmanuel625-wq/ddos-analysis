ad7b5517f08d8aee264d8852b13b7a2a447a2c2a
Emmanuel625-wq <imoodera@gmail.com>
2026-04-25 21:53:50 +0100

    Add files via upload
    Added file reading

--- ddos.py
+++ ddos.py
@@ -1,8 +1,17 @@
 #DDos Log Analysis Program
 
+def read_log_file(filename):
+    lines = []
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            line.append(line.rstrip("\n"))
+            return lines
+
+
+
+
 def main():
     pass
 
 if __name__ == "__main__":
     main()
-    
